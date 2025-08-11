29e727a30455143e89183d239404aefd97f2428f
greinerpampaenergia-web <greinerpampaenergia@gmail.com>
2025-08-11 12:14:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -250,3 +250,6 @@ def delete_progress(progress_id):
 # Create tables
 with app.app_context():
     db.create_all()
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=10000)
+
